34d67422a7e6c3ca5b100362e5a372a4b74ca0dd
iquicktest <iquicktest@gmail.com>
2013-07-21 16:33:11 +0800

    update 16

--- seleniumHello.py
+++ seleniumHello.py
@@ -9,7 +9,6 @@ class ETestCase(unittest.TestCase):
 	def test_example(self):
 		self.driver.get('http://www.baidu.com')
 		self.driver.find_element_by_name(name='wd').send_keys('iquicktest')
-		
 		self.assertTrue(self.driver.title.find('iquicktest')<0)
 	def tearDown(self):
 		self.driver.quit()
